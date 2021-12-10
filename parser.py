7ac9175ded259d83688f9f6128c6dbb6f05f4884
harold <harold@harold.com>
2021-12-09 18:24:41 -0800

    Upgrade table T0 complete

--- parser.py
+++ parser.py
@@ -4,9 +4,10 @@ import time
 from termcolor import colored
 import requests
 from columnar import columnar
+from click import style
 
 parser = argparse.ArgumentParser(description='RSI Store Parser')
-#parser.add_argument('-u', '--upgrades', help="Show all available upgrades", action="store_true")
+parser.add_argument('-u', '--upgrades', help="Show all available upgrades", action="store_true")
 parser.add_argument('-wd', '--watchdelay', dest='watchdelay', type=int, help='The delay you want to sniff the store at', default=10)
 args = parser.parse_args()
 
@@ -80,8 +81,16 @@ variables = """{
   ]
 }"""
 
-headers = ['name', 'price']
+headers = ['name', 'price', 'edition', 'unlimitedStock', 'availableStock', 'limitedTimeOffer']
 tableData = []
+storedJsonData = 0
+
+patterns = [
+    ('True', lambda text: style(text, fg='white', bg='green')),
+    ('False', lambda text: style(text, fg='white', bg='red')),
+    ('Warbond Edition', lambda text: style(text, fg='white', bg='yellow')),
+    ('.+Upgrade', lambda text: style(text, fg='white', bg='blue'))
+]
 
 setToken = requests.post(api_settoken)
 if setToken.status_code == 200:
@@ -94,7 +103,7 @@ else:
 #print(setTokenContext.status_code)
 #print(setTokenContext.text)
 
-while True:
+while args.upgrades:
     queryUpgradeSystem = requests.post(api_url, json={'query': filterShips, 'variables': variables}, cookies=setToken.cookies)
     if queryUpgradeSystem.status_code == 200:
         print("UPGRADE QUERY: PASS")
@@ -108,9 +117,9 @@ while True:
     #print(len(jsonData['data']['to']['ships']))
     buildIndex = 0
     for i in jsonData['data']['to']['ships']:
-        tableData.append([colored(i['skus'][0]['items'][0]['title'], "yellow"), i['skus'][0]['price']/100])
+        tableData.append([i['skus'][0]['items'][0]['title'], i['skus'][0]['price']/100, i['skus'][0]['title'], i['skus'][0]['unlimitedStock'], i['skus'][0]['availableStock'], i['skus'][0]['limitedTimeOffer']])
         buildIndex += 1
-    table = columnar(tableData, headers, no_borders=True)
+    table = columnar(tableData, headers,patterns=patterns, no_borders=False)
     print(table)
     
     time.sleep(args.watchdelay)
