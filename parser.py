34bcd5dab471c71a9254c9d79d5be87cac8bb7a4
harold <harold@harold.com>
2021-12-09 18:35:48 -0800

    Each ship can contain multiple SKUs for warbond/standard edition

--- parser.py
+++ parser.py
@@ -7,14 +7,15 @@ from columnar import columnar
 from click import style
 
 parser = argparse.ArgumentParser(description='RSI Store Parser')
-parser.add_argument('-u', '--upgrades', help="Show all available upgrades", action="store_true")
+parser.add_argument('-u', '--upgrade', help="Show all available upgrades", action="store_true")
+parser.add_argument('-s', '--standalone', help="Show all available standalone ships", action="store_true")
 parser.add_argument('-wd', '--watchdelay', dest='watchdelay', type=int, help='The delay you want to sniff the store at', default=10)
 args = parser.parse_args()
 
 api_settoken = "https://robertsspaceindustries.com/api/account/v2/setAuthToken"
 api_settokencontext = "https://robertsspaceindustries.com/api/ship-upgrades/setContextToken?fromShipId&pledgeId&toShipId&toSkuId"
 api_url = "https://robertsspaceindustries.com/pledge-store/api/upgrade/graphql"
-filterShips = """query filterShips($fromId: Int, $toId: Int, $fromFilters: [FilterConstraintValues], $toFilters: [FilterConstraintValues]) {
+query_upgrades = """query filterShips($fromId: Int, $toId: Int, $fromFilters: [FilterConstraintValues], $toFilters: [FilterConstraintValues]) {
   from(to: $toId, filters: $fromFilters) {
     ships {
       id
@@ -54,7 +55,7 @@ skus {
   }
 }
 """
-variables = """{
+variables_upgrades = """{
   "fromId": 0,
   "fromFilters": [],
   "toFilters": [
@@ -80,6 +81,209 @@ variables = """{
     }
   ]
 }"""
+query_standalone="""query GetBrowseListingQuery($storeFront: String, $query: SearchQuery) {
+  store(name: $storeFront, browse: true) {
+    listing: search(query: $query) {
+      resources {
+        ...TyItemFragment
+        __typename
+      }
+      __typename
+    }
+    __typename
+  }
+}
+
+fragment TyItemFragment on TyItem {
+  id
+  name
+  title
+  subtitle
+  url
+  body
+  excerpt
+  type
+  media {
+    thumbnail {
+      slideshow
+      storeSmall
+      __typename
+    }
+    list {
+      slideshow
+      __typename
+    }
+    __typename
+  }
+  nativePrice {
+    amount
+    discounted
+    discountDescription
+    __typename
+  }
+  price {
+    amount
+    discounted
+    taxDescription
+    discountDescription
+    __typename
+  }
+  stock {
+    ...TyStockFragment
+    __typename
+  }
+  tags {
+    name
+    __typename
+  }
+  ... on TySku {
+    label
+    customizable
+    isWarbond
+    isPackage
+    isVip
+    isDirectCheckout
+    __typename
+  }
+  ... on TyProduct {
+    skus {
+      id
+      isDirectCheckout
+      __typename
+    }
+    isVip
+    __typename
+  }
+  ... on TyBundle {
+    isVip
+    media {
+      thumbnail {
+        slideshow
+        __typename
+      }
+      __typename
+    }
+    discount {
+      ...TyDiscountFragment
+      __typename
+    }
+    __typename
+  }
+  __typename
+}
+
+fragment TyDiscountFragment on TyDiscount {
+  id
+  title
+  skus {
+    ...TyBundleSkuFragment
+    __typename
+  }
+  products {
+    ...TyBundleProductFragment
+    __typename
+  }
+  __typename
+}
+
+fragment TyBundleSkuFragment on TySku {
+  id
+  title
+  label
+  excerpt
+  subtitle
+  url
+  type
+  isWarbond
+  isDirectCheckout
+  media {
+    thumbnail {
+      storeSmall
+      slideshow
+      __typename
+    }
+    __typename
+  }
+  gameItems {
+    __typename
+  }
+  stock {
+    ...TyStockFragment
+    __typename
+  }
+  price {
+    amount
+    taxDescription
+    __typename
+  }
+  __typename
+}
+
+fragment TyStockFragment on TyStock {
+  unlimited
+  show
+  available
+  backOrder
+  qty
+  backOrderQty
+  level
+  __typename
+}
+
+fragment TyBundleProductFragment on TyProduct {
+  id
+  name
+  title
+  subtitle
+  url
+  type
+  excerpt
+  stock {
+    ...TyStockFragment
+    __typename
+  }
+  media {
+    thumbnail {
+      storeSmall
+      slideshow
+      __typename
+    }
+    __typename
+  }
+  nativePrice {
+    amount
+    discounted
+    __typename
+  }
+  price {
+    amount
+    discounted
+    taxDescription
+    __typename
+  }
+  skus {
+    ...TyBundleSkuFragment
+    __typename
+  }
+  __typename
+}"""
+variables_standalone="""{
+  "query": {
+    "skus": {
+      "products": [
+        "72"
+      ]
+    },
+    "limit": 2500,
+    "page": 1,
+    "sort": {
+      "field": "weight",
+      "direction": "desc"
+    }
+  },
+  "storeFront": "pledge"
+}
+"""
 
 headers = ['name', 'price', 'edition', 'unlimitedStock', 'availableStock', 'limitedTimeOffer']
 tableData = []
@@ -103,22 +307,32 @@ else:
 #print(setTokenContext.status_code)
 #print(setTokenContext.text)
 
-while args.upgrades:
-    queryUpgradeSystem = requests.post(api_url, json={'query': filterShips, 'variables': variables}, cookies=setToken.cookies)
-    if queryUpgradeSystem.status_code == 200:
-        print("UPGRADE QUERY: PASS")
-    else:
-        print("UPGRADE QUERY: {}".format(queryUpgradeSystem.status_code))
-    #print(queryUpgradeSystem.text)
-
-    jsonData = queryUpgradeSystem.json()
+while True:
+    if args.upgrade:
+        queryUpgradeSystem = requests.post(api_url, json={'query': query_upgrades, 'variables': variables_upgrades}, cookies=setToken.cookies)
+        if queryUpgradeSystem.status_code == 200:
+            print("UPGRADE QUERY: PASS")
+        else:
+            print("UPGRADE QUERY: {}".format(queryUpgradeSystem.status_code))
+        jsonData = queryUpgradeSystem.json()
+    if args.standalone:
+        queryUpgradeSystem = requests.post(api_url, json={'query': query_standalone, 'variables': variables_standalone}, cookies=setToken.cookies)
+        if queryUpgradeSystem.status_code == 200:
+            print("STANDALONE QUERY: PASS")
+        else:
+            print("STANDALONE QUERY: {}".format(queryUpgradeSystem.status_code))
+        jsonData = queryUpgradeSystem.json()
     #for i in jsonData['data']['to']['ships']:
     #    print("{}\n-${}".format(colored(i['skus'][0]['items'][0]['title'], "yellow"), i['skus'][0]['price']/100))
     #print(len(jsonData['data']['to']['ships']))
-    buildIndex = 0
     for i in jsonData['data']['to']['ships']:
-        tableData.append([i['skus'][0]['items'][0]['title'], i['skus'][0]['price']/100, i['skus'][0]['title'], i['skus'][0]['unlimitedStock'], i['skus'][0]['availableStock'], i['skus'][0]['limitedTimeOffer']])
-        buildIndex += 1
+        for j in i['skus']:
+            tableData.append([j['items'][0]['title'],
+            j['price']/100,
+            j['title'],
+            j['unlimitedStock'],
+            j['availableStock'],
+            j['limitedTimeOffer']])
     table = columnar(tableData, headers,patterns=patterns, no_borders=False)
     print(table)
     
